d3cc23c1c871201a7a1fa276a64fbcb2de5e243f
Bruno Windels <bruno.windels@gmail.com>
2016-11-06 23:42:12 +0100

    first commit

--- async_event_source/ViewController.swift
+++ async_event_source/ViewController.swift
@@ -7,9 +7,64 @@
 //
 
 import Cocoa
+import Dispatch;
+
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var output: NSTextField!
+    @IBOutlet weak var input: NSTextField!
+    @IBOutlet weak var animation: NSProgressIndicator!
+    
+    @IBOutlet weak var button: NSButton!
+    
+    var source: DispatchSourceUserDataAdd? = nil;
+    var max: Int32 = 0;
+    var thread: Thread? = nil;
+    
+    @IBAction func startCountdownPressed(_ sender: AnyObject) {
+        source = DispatchSource.makeUserDataAddSource(queue: DispatchQueue.main);
+        
+        func sourceCallback() {
+            let value = output.intValue - 1;
+            output.intValue = value;
+            if value == 0 {
+                cleanup();
+            }
+        }
+        
+        source!.setEventHandler(handler: sourceCallback);
+        source!.resume();
+        max = input.intValue;
+        thread = Thread(target: self, selector: #selector(runCountdown), object: nil);
+        
+        
+        animation.startAnimation(self);
+        output.intValue = max;
+        button.isEnabled = false;
+        thread!.start();
+    }
+    
+    func cleanup() {
+        thread = nil;
+        source!.cancel();
+        source = nil;
+        max = 0;
+
+        animation.stopAnimation(self);
+        button.isEnabled = true;
+
+    }
+    
+    func runCountdown() {
+        var value : Int32 = max;
+        while (value > 0) {
+            sleep(1);
+            source!.add(data: 1);
+            value -= 1;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
