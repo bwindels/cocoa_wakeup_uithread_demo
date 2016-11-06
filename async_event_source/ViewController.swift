d73b1f38b57f02919c0770039344ba7553b64d72
Bruno Windels <bruno.windels@gmail.com>
2016-11-06 21:53:50 +0100

    Initial Commit

---
+++ async_event_source/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  async_event_source
+//
+//  Created by Bruno Windels on 06/11/16.
+//  Copyright © 2016 Bruno Windels. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
